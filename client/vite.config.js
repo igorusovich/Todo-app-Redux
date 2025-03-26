2618bd414d554bc0b2339f460e9bcec45b8faf6f
igor.tech <usovigorok@gmail.com>
2025-03-26 10:42:14 +0000

    Update config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    allowedHosts: "all"
+    allowedHosts: ["c7bcfc2d-3d01-4873-97ac-508f31153247-00-kku2ioxkvf7u.worf.replit.dev"]
   },
 });
